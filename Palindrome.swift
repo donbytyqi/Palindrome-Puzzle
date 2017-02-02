3a21446499629aa2303ec3520df4bd518ee4fa9c
Don Bytyqi <donbytyqiapps@gmail.com>
2017-02-02 15:04:44 +0100

    Update Palindrome.swift

--- Palindrome.swift
+++ Palindrome.swift
@@ -1,16 +1,12 @@
 //this is probably not very efficient and there's most likely a better way to do it ¯\_(ツ)_/¯
 
 func findPalindrome(_ string: String) {
-    let characters = string.characters.reversed()
-    var result = ""
-    for char in characters {
-        result += "\(char)"
-    }
+    let result = String(string.characters.reversed())
     if result == string {
-        print("It's a palindrome because \(string) is spelled the same backwards and forwards \(result)")
+        print("It's a palindrome")
     }
     else {
-        print("It's not a palindrome because \(string) is not spelled the same backwards and forwards \(result)")
+        print("It's not")
     }
 }
 
